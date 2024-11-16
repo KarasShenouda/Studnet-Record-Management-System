09d5efd64ad11899c01d16082d0353cb428e9621
Karas Shenouda <karasshenouda139@gmail.com>
2024-11-16 22:34:49 +0200

    first commit

---
+++ test/the_final_project_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:the_final_project_dart/the_final_project_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
